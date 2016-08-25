0240e45a10427eb20cd6d2e4fd905b6d8e3e21a5
ldavidgomez <ldavid.github@gmail.com>
2016-08-25 13:36:43 +0200

    added topics example

--- rabbitMQ/Topics/emit_log_topic.php
+++ rabbitMQ/Topics/emit_log_topic.php
@@ -11,18 +11,17 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('172.17.0.2', 5672, 'guest', 'guest');
-
 $channel = $connection->channel();
-$channel->exchange_declare('topic_logs', 'direct', false, false, false);
+
+$channel->exchange_declare('topic_logs', 'topic', false, false, false);
 
 $routing_key = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'anonymous.info';
-
 $data = implode(' ', array_slice($argv, 2));
-
 if(empty($data)) $data = "Hello World!";
+
 $msg = new AMQPMessage($data);
 
-$channel->basic_publish($msg, 'topic_logs', $severity);
+$channel->basic_publish($msg, 'topic_logs', $routing_key);
 
 echo " [x] Sent ",$routing_key,':',$data," \n";
 
