0b387e67f90ee9ce21ecccd1b6d9c5d83a1b4374
ldavidgomez <ldavid.github@gmail.com>
2016-08-25 13:36:26 +0200

    added topics example

---
+++ rabbitMQ/Topics/emit_log_topic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 25/08/16
+ * Time: 13:10
+ */
+require_once '../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('172.17.0.2', 5672, 'guest', 'guest');
+
+$channel = $connection->channel();
+$channel->exchange_declare('topic_logs', 'direct', false, false, false);
+
+$routing_key = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'anonymous.info';
+
+$data = implode(' ', array_slice($argv, 2));
+
+if(empty($data)) $data = "Hello World!";
+$msg = new AMQPMessage($data);
+
+$channel->basic_publish($msg, 'topic_logs', $severity);
+
+echo " [x] Sent ",$routing_key,':',$data," \n";
+
+$channel->close();
+$connection->close();
---
+++ rabbitMQ/Topics/receive_logs_topic.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 25/08/16
+ * Time: 13:11
+ */
+require_once '../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('172.17.0.2', 5672, 'guest', 'guest');
+
+$channel = $connection->channel();
+$channel->exchange_declare('topic_logs', 'topic', false, false, false);
+
+list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
+
+$severities = array_slice($argv, 1);
+
+$binding_keys = array_slice($argv, 1);
+if( empty($binding_keys )) {
+    file_put_contents('php://stderr', "Usage: $argv[0] [binding_key]\n");
+    exit(1);
+}
+
+foreach($binding_keys as $binding_key) {
+    $channel->queue_bind($queue_name, 'topic_logs', $binding_key);
+}
+
+echo ' [*] Waiting for logs. To exit press CTRL+C', "\n";
+
+$callback = function($msg){
+    echo ' [x] ',$msg->delivery_info['routing_key'], ':', $msg->body, "\n";
+};
+
+$channel->basic_consume($queue_name, '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
